81fe4035b5a81775651de76c61b8cb340616ee0a
Nico28gm <gar25348@uvg.edu.gt>
2025-09-05 09:20:53 -0600

    Agregué los atributos de Objeto, Usuario, Administrador y Premio

---
+++ Premio.java
@@ -0,0 +1,7 @@
+public class Premio 
+{
+    private String nombre;
+    private String descripcion;
+    private int puntos;
+
+}
---
+++ Usuario.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Usuario 
+{
+    private String nombre;
+    private String correoInstitucional;
+    private int carnet;
+    private int puntos;
+    private ArrayList<RegistroObjetos> historialAcciones;
+    private ArrayList<Premio> premioReclamado;
+    private boolean permiso = false;
+}
