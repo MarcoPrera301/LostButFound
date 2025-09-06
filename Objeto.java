bb107b583cbabb0cd7f1720a05a6402a3f05544c
Bini960 <castroandrestruco@gmail.com>
2025-09-05 23:18:23 -0600

    Creacion del constructor incial de la clase

--- Objeto.java
+++ Objeto.java
@@ -12,4 +12,14 @@ public class Objeto
     private String reportadoPor;
     private String usuarioQueReclama;
 
+    public Objeto(String descripcion, String tipo, String estado, LocalDate fechaEncontrado, String lugarEncontrado, int id, String reportadoPor) {
+        this.descripcion = descripcion;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.fechaEncontrado = fechaEncontrado;
+        this.lugarEncontrado = lugarEncontrado;
+        this.id = id;
+        this.reportadoPor = reportadoPor;
+        this.usuarioQueReclama = null; 
+    }
 }
