18a43e5bfbb8ca529c85ae374c15b03be808c9d1
Nico28gm <gar25348@uvg.edu.gt>
2025-09-08 21:09:58 -0600

    Constructor y getters de Premio.java

--- Premio.java
+++ Premio.java
@@ -4,4 +4,16 @@ public class Premio
     private String descripcion;
     private int puntos;
 
+
+    
+    public Premio(String nombre, String descripcion, int puntos) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
+    public int getPuntos() { return puntos; }
+
 }
