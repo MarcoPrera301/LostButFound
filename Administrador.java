5abd0781a083b7e0aa078acae7ff8cd5fe748c4b
Xhablax <juanitolou06@gmail.com>
2025-09-08 18:50:19 -0600

    Update Administrador.java

--- Administrador.java
+++ Administrador.java
@@ -26,3 +26,18 @@ public class Administrador
         }
         return t;
     }
+    private static String[] splitCsv(String line, int cols) {
+        String[] out = new String[cols];
+        int idx = 0; StringBuilder cur = new StringBuilder(); boolean inQ = false;
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (c == '\"') {
+                if (inQ && i + 1 < line.length() && line.charAt(i + 1) == '\"') { cur.append('\"'); i++; }
+                else inQ = !inQ;
+            } else if (c == ',' && !inQ) { out[idx++] = cur.toString(); cur.setLength(0); }
+            else cur.append(c);
+        }
+        out[idx++] = cur.toString();
+        while (idx < cols) out[idx++] = "";
+        return out;
+    }
